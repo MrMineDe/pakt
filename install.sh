d2fc3173b7db4d847ac37f0e46f38bf9f2102dff
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2024-09-01 09:03:24 +0200

    Fixed install scripts

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ echo "Putting executable at /usr/bin/pakt ..."
 install -m 755 pakt /usr/bin/pakt
 
 echo "Installing the man pages ..."
-install -m 644 pakt.1 /usr/share/man/man1/pakt.1
-install -m 644 pakt-sync.1 /usr/share/man/man1/pakt-sync.1
+gzip pakt.1
+install -m 644 pakt.1.gz /usr/share/man/man1/pakt.1.gz
+gzip pakt-sync.1
+install -m 644 pakt-sync.1.gz /usr/share/man/man1/pakt-sync.1.gz
 
 echo "Putting default config at /etc/pacman.conf.default ..."
 install -m 644 pakt.conf.default /etc/pakt.conf.default
--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,29 @@ echo "Uninstalling pakt ..."
 echo "Removing executable at /usr/bin/pakt ..."
 rm /usr/bin/pakt
 
-echo "Removing default config at /etc/pacman.conf.example ..."
-rm /etc/pakt.conf.example
+echo "Removing man pages ..."
+rm /usr/share/man/man1/pakt.1
+rm /usr/share/man/man1/pakt-sync.1
 
-echo "NOTE: ~/.config/pakt still exists!"
+echo "Removing default config at /etc/pacman.conf.example ..."
+rm /etc/pakt.conf.default
+
+while true; do
+	reap -p "Do you want to remove your config at /etc/pakt.conf? [Y/n]" response
+
+	case "$response" in
+		[Yy]|[Yy][Ee][Ss]|"")
+			echo "Removing /etc/pakt.conf ..."
+			rm /etc/pakt.conf
+			break
+			;;
+		[Nn]|[Nn][Oo])
+			break
+			;;
+		*)
+			echo "Invalid input. Please enter 'y' or 'n'."
+			;;
+	esac
+done
 
 echo "Goodbye :["
